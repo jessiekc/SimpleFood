136d406d989c5f77bef1a9002fd692e62a936bf7
Zhengdai Hu (David) <hu.zhengdai@gmail.com>
2018-11-26 00:16:22 -0600

    Created skeleton for MainActivity and RestaurantsFragment

--- app/src/main/java/com/cityscholar/cs465/simplefood/MainActivity.java
+++ app/src/main/java/com/cityscholar/cs465/simplefood/MainActivity.java
@@ -1,13 +1,31 @@
 package com.cityscholar.cs465.simplefood;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPagerRestaurants = findViewById(R.id.viewPagerRestaurants);
+        viewPagerRestaurants.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return RestaurantsFragment.newInstance("1", "2");
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+        });
+        viewPagerRestaurants.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener());
     }
 }
+
