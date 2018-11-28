d912379d8520b3b7a51a1a44f41a1ba748c33c24
Zhengdai Hu (David) <hu.zhengdai@gmail.com>
2018-11-28 02:14:09 -0600

    Give examples of restaurants.

--- app/src/main/java/com/cityscholar/cs465/simplefood/ExampleRestaurants.java
+++ app/src/main/java/com/cityscholar/cs465/simplefood/ExampleRestaurants.java
@@ -1,7 +1,6 @@
 package com.cityscholar.cs465.simplefood;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class ExampleRestaurants {
@@ -9,7 +8,7 @@ public class ExampleRestaurants {
             "This restaurants is very close",
             "This is a Chinese restaurants",
             "This restaurants is similar to what you've been to");
-    public static final List<Restaurant> RESTAURANTS = Collections.nCopies(10,
+    public static final List<Restaurant> RESTAURANTS = Arrays.asList(
             new Restaurant.Builder()
                     .setName("Name")
                     .setCover(R.drawable.restaurant_img1)
@@ -20,6 +19,18 @@ public class ExampleRestaurants {
                     .appendDetail("Restaurant detail content...Restaurant detail content...Restaurant detail content...Restaurant detail content...Restaurant detail content...")
                     .appendDetail(R.drawable.restaurant_detail_img1, "IMG1")
                     .appendDetail(R.drawable.restaurant_detail_img2, "IMG2")
+                    .build(),
+            new Restaurant.Builder()
+                    .setName("Name")
+                    .setCover(R.drawable.restaurant_img1)
+                    .putHighlight(FilterType.PRICE, highlights.get(FilterType.PRICE))
+                    .putHighlight(FilterType.DISTANCE, highlights.get(FilterType.DISTANCE))
+                    .putHighlight(FilterType.CUISINE, highlights.get(FilterType.CUISINE))
+                    .putHighlight(FilterType.FAMILIARITY, highlights.get(FilterType.FAMILIARITY))
+                    .appendDetail(R.drawable.restaurant_detail_img1, "IMG1")
+                    .appendDetail("Restaurant detail content...Restaurant detail content...Restaurant detail content...Restaurant detail content...Restaurant detail content...")
+                    .appendDetail("Restaurant detail content...Restaurant detail content...Restaurant detail content...Restaurant detail content...Restaurant detail content...")
+                    .appendDetail(R.drawable.restaurant_detail_img2, "IMG2")
                     .build()
-    );
+            );
 }
